d4f3cdc49c2c6db6de5d33e670dd3e95131ddba6
Keaton Bruce <keaton@nucypher.com>
2018-06-25 18:16:12 -0600

    Add many features to boringssl.go (see full message)
    The changes are listed in file order.
        *Change globalNid to SECP256K1.
        *Add type definitions for various boringssl types.
        *Add SetECGroup, SetECOrder, GetECGroup, and GetECOrder.
        *Remove GetECOrderByGlobalGroup because SetECOrder replaces it.
        *Change TmpBNMontCTX to constant time.
        *Add BytesToBN and make BigIntToBN use it.
        *Add BNToBytes and SizeOfBN.
        *Add AddBN, SubBN, and the rest of the operations for BNs.

--- libs/boringssl/boringssl.go
+++ libs/boringssl/boringssl.go
@@ -18,8 +18,16 @@ import (
 )
 
 // secp256k1 is the constant curve nid 714
-const globalNid = 714
-var globalGroup *C.EC_GROUP
+const SECP256K1 = 714
+var globalGroup ECGroup
+var globalOrder ECOrder
+
+type BigNum *C.BIGNUM
+type BNCtx *C.BN_CTX
+type BNMontCtx *C.BN_MONT_CTX
+type ECGroup *C.EC_GROUP
+type ECOrder *C.BIGNUM
+type ECPoint *C.EC_POINT
 
 func GetBigNum() *C.BIGNUM {
     // bNum must be freed later by the calling function.
@@ -27,14 +35,42 @@ func GetBigNum() *C.BIGNUM {
     return bNum
 }
 
-func SetECGroupByCurveNid() {
+func SetECGroup() {
     if unsafe.Pointer(globalGroup) == C.NULL {
-        globalGroup = C.EC_GROUP_new_by_curve_name(C.int(globalNid))
+        globalGroup = C.EC_GROUP_new_by_curve_name(C.int(SECP256K1))
         if unsafe.Pointer(globalGroup) == C.NULL {
             // Failure
             log.Fatal("Curve group lookup failed")
         }
     }
+}
+
+func SetECOrder() {
+    if unsafe.Pointer(globalOrder) == C.NULL {
+        ecOrder := GetBigNum()
+        var bnCtx *C.BN_CTX = C.BN_CTX_new()
+        defer FreeBNCTX(bnCtx)
+        result := int(C.EC_GROUP_get_order(globalGroup, ecOrder, bnCtx))
+        if result != 1 {
+            // Failure
+            log.Fatal("Curve order lookup failed")
+        }
+        globalOrder = ecOrder
+    }
+}
+
+func GetECGroup() *C.EC_GROUP {
+    if unsafe.Pointer(globalGroup) == C.NULL {
+        SetECGroup()
+    }
+    return globalGroup
+}
+
+func GetECOrder() *C.BIGNUM {
+    if unsafe.Pointer(globalOrder) == C.NULL {
+        SetECOrder()
+    }
+    return globalOrder
 }
 
 func GetECGroupByCurveNid(curveNid int) *C.EC_GROUP {
@@ -58,21 +94,6 @@ func GetECOrderByCurveNid(curveNid int) *C.BIGNUM {
     defer FreeBNCTX(ctx)
 
     result := int(C.EC_GROUP_get_order(ecGroup, ecOrder, ctx))
-    if result != 1 {
-        // Failure
-        log.Fatal("Curve order lookup failed")
-    }
-    return ecOrder
-}
-
-func GetECOrderByGlobalGroup() *C.BIGNUM {
-    // ecOrder must be freed later by the calling function.
-    var ecOrder *C.BIGNUM = GetBigNum()
-
-    var ctx *C.BN_CTX = C.BN_CTX_new()
-    defer FreeBNCTX(ctx)
-
-    result := int(C.EC_GROUP_get_order(globalGroup, ecOrder, ctx))
     if result != 1 {
         // Failure
         log.Fatal("Curve order lookup failed")
@@ -186,16 +207,12 @@ func GetAffineCoordsFromECPoint(ecPoint *C.EC_POINT, ecGroup *C.EC_GROUP, curveN
 
 func TmpBNMontCTX(modulus *C.BIGNUM) *C.BN_MONT_CTX {
     // montCtx must be freed later by the calling function.
-    montCtx := C.BN_MONT_CTX_new()
-    if unsafe.Pointer(montCtx) == C.NULL {
-        log.Fatal("New Montgomery CTX failure")
-    }
     ctx := C.BN_CTX_new()
     defer FreeBNCTX(ctx)
 
-    result := C.BN_MONT_CTX_set(montCtx, modulus, ctx)
-    if result != 1 {
-        log.Fatal("Montgomery CTX set with modulus failure")
+    montCtx := C.BN_MONT_CTX_new_consttime(modulus, ctx)
+    if unsafe.Pointer(montCtx) == C.NULL {
+        log.Fatal("New Montgomery CTX set with modulus failure")
     }
     return montCtx
 }
@@ -207,19 +224,37 @@ func IntToBN(sInt int) *C.BIGNUM {
 
 func BigIntToBN(bInt *big.Int) *C.BIGNUM {
     goIntAsBytes := bInt.Bytes()
+    return BytesToBN(goIntAsBytes)
+}
 
-    cIntAsBytes := C.CBytes(goIntAsBytes)
-    defer C.free(cIntAsBytes)
-
+func BytesToBN(bytes []byte) *C.BIGNUM {
+    cBytes := C.CBytes(bytes)
+    defer C.free(cBytes)
     // cBN must be freed later by the calling function.
-    var cBN *C.BIGNUM = C.BN_bin2bn((*C.uint8_t)(cIntAsBytes),
-        C.size_t(len(goIntAsBytes)),
-        GetBigNum())
+    var cBN *C.BIGNUM = C.BN_bin2bn((*C.uint8_t)(cBytes), C.size_t(len(bytes)), GetBigNum())
     if unsafe.Pointer(cBN) == C.NULL {
         // Failure
         log.Fatal("Allocation failure")
     }
     return cBN
+}
+
+func BNToBytes(cBN *C.BIGNUM) []byte {
+    size := SizeOfBN(cBN)
+    space := make([]byte, size)
+    cSpace := C.CBytes(space)
+    defer C.free(cSpace)
+
+    written := C.BN_bn2bin(cBN, (*C.uint8_t)(cSpace))
+    if int(written) != size {
+        log.Fatal("Size of written doesn't equal size of bignum")
+    }
+    bytes := C.GoBytes(cSpace, C.int(size))
+    return bytes
+}
+
+func SizeOfBN(cBN *C.BIGNUM) int {
+    return int(C.BN_num_bytes(cBN))
 }
 
 func CompareBN(cBN1, cBN2 *C.BIGNUM) int {
@@ -238,6 +273,147 @@ func MultiplyBN(cBN1, cBN2 *C.BIGNUM) *C.BIGNUM {
         log.Fatal("Multiplication failure")
     }
     return newBN
+}
+
+func AddBN(cBN1, cBN2 *C.BIGNUM) *C.BIGNUM {
+    // newBN must be free later by the calling function.
+    newBN := GetBigNum()
+    result := C.BN_add(newBN, cBN1, cBN2)
+    if result != 1 {
+        log.Fatal("Addition failure")
+    }
+    return newBN
+}
+
+func SubBN(cBN1, cBN2 *C.BIGNUM) *C.BIGNUM {
+    // newBN must be free later by the calling function.
+    newBN := GetBigNum()
+    result := C.BN_sub(newBN, cBN1, cBN2)
+    if result != 1 {
+        log.Fatal("Subtraction failure")
+    }
+    return newBN
+}
+
+func DivBN(numerator, divisor *C.BIGNUM) (*C.BIGNUM, *C.BIGNUM) {
+    quotient := GetBigNum()
+    rem := GetBigNum()
+
+    bnCtx := C.BN_CTX_new()
+    defer C.BN_CTX_free(bnCtx)
+
+    result := C.BN_div(quotient, rem, numerator, divisor, bnCtx)
+    if result != 1 {
+        log.Fatal("Division failure")
+    }
+    return quotient, rem
+}
+
+func ModBN(numerator, divisor *C.BIGNUM) *C.BIGNUM {
+    bnCtx := C.BN_CTX_new()
+    defer C.BN_CTX_free(bnCtx)
+    // This is equivalent to C.BN_mod(bignum, hashDigest, orderMinusOne, bnCtx)
+    // but it uses BN_div because it isn't a macro.
+    quotient, rem := DivBN(numerator, divisor)
+    defer FreeBigNum(quotient)
+    return rem
+}
+
+func ModExpBN(cBN1, cBN2, mod *C.BIGNUM) *C.BIGNUM {
+    /*
+    Performs a BN_mod_exp on two BIGNUMS.
+    WARNING: Only in constant time if BN_FLG_CONSTTIME is set on the BN.
+    */
+    power := GetBigNum()
+    defer FreeBigNum(power)
+
+    bnCtx := C.BN_CTX_new()
+    defer FreeBNCTX(bnCtx)
+
+    bnMontCtx := TmpBNMontCTX(mod)
+    result := C.BN_mod_exp_mont_consttime(power,
+        cBN1, cBN2, mod, bnCtx, bnMontCtx)
+
+    if result != 1 {
+        log.Fatal("BN_mod_exp failure")
+    }
+
+    return power
+}
+
+func ModMulBN(cBN1, cBN2, mod *C.BIGNUM) *C.BIGNUM {
+    /*
+    Performs a BN_mod_mul between two BIGNUMS.
+    */
+    product := GetBigNum()
+
+    bnCtx := C.BN_CTX_new()
+    defer FreeBNCTX(bnCtx)
+
+    result := C.BN_mod_mul(product, cBN1, cBN2, mod, bnCtx)
+    if result != 1 {
+        log.Fatal("BN_mod_mul failure")
+    }
+    return product
+}
+
+func ModInverseBN(cBN, mod *C.BIGNUM) *C.BIGNUM {
+    product := GetBigNum()
+
+    bnCtx := C.BN_CTX_new()
+    defer FreeBNCTX(bnCtx)
+
+    // NULL is used in place of out_no_inverse because
+    // the inverse should always exist for SECP256K1.
+    result := C.BN_mod_inverse_blinded(product, (*C.int)(C.NULL), cBN, TmpBNMontCTX(mod), bnCtx)
+
+    if result != 1 {
+        log.Fatal("BN_mod_inverse failure")
+    }
+
+    return product
+}
+
+func ModAddBN(cBN1, cBN2, mod *C.BIGNUM) *C.BIGNUM {
+    sum := GetBigNum()
+
+    bnCtx := C.BN_CTX_new()
+    defer FreeBNCTX(bnCtx)
+
+    result := C.BN_mod_add(sum, cBN1, cBN2, mod, bnCtx)
+    if result != 1 {
+        log.Fatal("BN_mod_add failure")
+    }
+
+    return sum
+}
+
+func ModSubBN(cBN1, cBN2, mod *C.BIGNUM) *C.BIGNUM {
+    sub := GetBigNum()
+
+    bnCtx := C.BN_CTX_new()
+    defer FreeBNCTX(bnCtx)
+
+    result := C.BN_mod_sub(sub, cBN1, cBN2, mod, bnCtx)
+    if result != 1 {
+        log.Fatal("BN_mod_sub failure")
+    }
+
+    return sub
+}
+
+func NNModBN(cBN1, cBN2 *C.BIGNUM) *C.BIGNUM {
+    rem := GetBigNum()
+
+    bnCtx := C.BN_CTX_new()
+    defer FreeBNCTX(bnCtx)
+
+    result := C.BN_nnmod(rem, cBN1, cBN2, bnCtx)
+    if result != 1 {
+        log.Fatal("BN_mod_sub failure")
+    }
+
+    return rem
 }
 
 func BNToDecStr(cBN *C.BIGNUM) string {
